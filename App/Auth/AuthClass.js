d81bcc95c8c83951e38fd6dd13b23474e02ce7b5
nchavchanidze <nikolozchavchanidze@gmail.com>
2021-10-07 23:41:19 +0400

    Auth page

--- App/Auth/AuthClass.js
+++ App/Auth/AuthClass.js
@@ -1,1 +1,19 @@
-console.log("this is Auth");
+$(function () {
+  let forgotQuestion = $(".auth-q-forgot");
+  registerQuestion = $(".auth-q-register");
+  forgotContainer = $(".password-reset-container");
+  registerContainer = $(".register-container");
+  loginContainer = $(".login-container");
+
+  $(forgotQuestion).click(function () {
+    $(loginContainer).addClass("hide");
+    $(forgotContainer).addClass("show");
+    console.log("Its working");
+  });
+
+  $(registerQuestion).click(function () {
+    $(loginContainer).addClass("hide");
+    $(registerContainer).addClass("show");
+    console.log("Its working 2");
+  });
+});
