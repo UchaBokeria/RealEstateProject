21b5826c116506b33fb28ae81cfc56d41dea1bc4
nchavchanidze <nikolozchavchanidze@gmail.com>
2021-10-09 16:14:14 +0400

    Add Estate Page

--- App/Estate/EstateClass.js
+++ App/Estate/EstateClass.js
@@ -1,15 +1,18 @@
-console.log("this is Estate");
-var newslider = new Slider({
-    data: { route: "Home", act: "read" },
-    area: "#mySlider",
-    type: "slideshow",
-    buttons: {
-        left: true,
-        right: true,
-    },
-    scrollHide: true,
-    cell: {
-        class: ["sa"],
-        gap: "4vw"
+$(function () {
+  $(".toggle-btn-value").click(function () {
+    var mainParent = $(this).parent(".toggle-btn");
+    if ($(mainParent).find("input.toggle-btn-value").is(":checked")) {
+      $(mainParent).addClass("active");
+    } else {
+      $(mainParent).removeClass("active");
     }
+  });
+  let statementsContainer = $(".statements-container");
+  statementCard = $(".statement-card");
+  statementViewBtn = $(".estate-view");
+
+  $(statementViewBtn).click(function () {
+    $(statementsContainer).toggleClass("v-statements-container");
+    $(statementCard).toggleClass("vertical");
+  });
 });
